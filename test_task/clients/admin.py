91ef1b3e0f4f1f31f47b7b62aca1354ffd24822a
opqx <zenchenko@protonmail.com>
2018-10-04 22:48:30 +0300

    добавил поиск в админке

--- test_task/clients/admin.py
+++ test_task/clients/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Contact
 
 
-admin.site.register(Contact)
+# admin.site.register(Contact)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ('name', 'company', 'email', 'phone', 'interest')
+    search_fields = ['name', 'company', 'email', 'phone', 'interest']
+
+
+admin.site.register(Contact, ContactAdmin)
