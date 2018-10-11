d12b2dd03e1346ba39367e7b958094876a4a5cae
opqx <zenchenko@protonmail.com>
2018-10-11 10:33:25 +0300

    добавил еирархическую структуру

--- test_task/clients/admin.py
+++ test_task/clients/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, Companies, Country, Region, City, Street, Building, Office
 
 
-# admin.site.register(Contact)
+class CountryInline(admin.StackedInline):
+    model = Country
+
+
+class RegionInline(admin.StackedInline):
+    model = Region
+
+
+
+class CityInline(admin.StackedInline):
+    model = City
+
+
+class StreetInline(admin.StackedInline):
+    model = Street
+
+
+class BuildingInline(admin.StackedInline):
+    model = Building
+
+
+class OfficeInline(admin.StackedInline):
+    model = Office
+
+
+class CompaniesAdmin(admin.ModelAdmin):
+    list_display = ['company', 'new_col', ]
+    
+    def new_col(self, obj):
+        return "add_new_col"
+
+    inlines = [CountryInline, RegionInline, CityInline, StreetInline, BuildingInline, OfficeInline]
 
 
 class ContactAdmin(admin.ModelAdmin):
@@ -11,4 +42,13 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = ['name', 'company', 'email', 'phone', 'interest']
 
 
+admin.site.register(Companies, CompaniesAdmin)
 admin.site.register(Contact, ContactAdmin)
+
+
+admin.site.register(Country, admin.ModelAdmin)
+admin.site.register(Region, admin.ModelAdmin)
+admin.site.register(City, admin.ModelAdmin)
+admin.site.register(Street, admin.ModelAdmin)
+admin.site.register(Building, admin.ModelAdmin)
+admin.site.register(Office, admin.ModelAdmin)
--- test_task/clients/models.py
+++ test_task/clients/models.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.urls import reverse
 
 
-class Contact(models.Model):
-    name = models.CharField(max_length=200)
+class Companies(models.Model):
     company = models.CharField(max_length=200) 
+    
+    def __str__(self):
+        return self.company
+
+class Contact(models.Model):
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
     interest = models.CharField(max_length=200)
@@ -17,3 +23,62 @@ class Contact(models.Model):
 
     def get_absolute_url(self):
         return reverse('update', kwargs={'pk': self.pk})
+
+
+class Country(models.Model):
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    name = models.CharField("Country", max_length=50)
+
+    
+    def __str__(self):
+        return self.name    
+
+class Region(models.Model):
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField("Region", max_length=50)
+
+    
+    def __str__(self):
+        return self.name    
+    
+
+class City(models.Model):
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    name = models.CharField("City", max_length=50)
+
+    
+    def __str__(self):
+        return self.name    
+    
+
+class Street(models.Model):
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    name = models.CharField("Street", max_length=50)
+
+    
+    def __str__(self):
+        return self.name    
+    
+
+class Building(models.Model):
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    street = models.ForeignKey(Street, on_delete=models.CASCADE)
+    name = models.CharField("Building", max_length=50)
+
+    
+    def __str__(self):
+        return self.name    
+    
+
+class Office(models.Model):
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    name = models.CharField("Office", max_length=50)
+
+    
+    def __str__(self):
+        return self.name    
+
